777e031ddd81f78ec779aaafe9482ae17ab288ec
gaodawei <gaodawei@linkme.org.cn>
2019-03-26 04:05:47 +0800

    添加TheadPoolKey

--- src/main/java/demo/ms/project/controller/DummyController.java
+++ src/main/java/demo/ms/project/controller/DummyController.java
@@ -9,20 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DummyController {
 
-    @HystrixCommand(commandKey = "DummyNumber")
+    @HystrixCommand(commandKey = "DummyNumber",threadPoolKey = "DummyNumberPool")
     @GetMapping("/number")
     public int number(int a,int b){
         return a/b;
     }
 
-    @HystrixCommand(commandKey = "DummyTimeout")
+    @HystrixCommand(commandKey = "DummyTimeout",threadPoolKey = "DummyTimeoutPool")
     @GetMapping("/timeout")
     public String timeout() throws InterruptedException {
         Thread.sleep(2000);
         return "OK";
     }
 
-    @HystrixCommand(commandKey = "DummyHello",fallbackMethod = "helloFallback")
+    @HystrixCommand(commandKey = "DummyHello",,threadPoolKey = "DummyHelloPool",fallbackMethod = "helloFallback")
     @GetMapping("/hello")
     public String hello(String name){
         return "你好:" + name;
